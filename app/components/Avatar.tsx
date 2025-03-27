28649b2c1e0e71e39bdbd206bb8c9a14722d3438
rohit <rs8918027@gmail.com>
2025-03-27 23:56:15 +0530

    first commit

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,18 @@
+'use client'
+import Image from 'next/image'
+
+interface AvatarProps{
+    src?: string | null | undefined
+}
+
+const Avatar: React.FC<AvatarProps> = ({
+    src
+}) => {
+  return (
+    <div>
+        <Image className='rounded-full' height="30" width="30" alt="Avatar" src={src || '/images/placeholder.jpeg'} />
+    </div>
+  )
+}
+
+export default Avatar
---
+++ app/types/index.ts
@@ -0,0 +1,21 @@
+import { Listing, User, Reservation } from "@prisma/client"
+
+export type safeListing = Omit<Listing, "createdAt"> & {
+    createdAt: string;
+}
+
+export type SafeUser = Omit<
+    User,
+    "createdAt" | "updatedAt" | "emailVerified"
+> & {
+    createdAt: string;
+    updatedAt: string;
+    emailVerified: string | null;
+}
+
+export type SafeReservation = Omit<Reservation, "createdAt" | 'startDate' | "endDate" | "listing"> & {
+    createdAt: string;
+    startDate: string;
+    endDate: string;
+    listing: safeListing;
+}
